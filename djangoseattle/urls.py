ef8b4aac8a74bb57a8983755bb7c921ecdbb802e
Alec Koumjian <akoumjian@gmail.com>
2013-04-11 18:49:54 -0700

    index with styles

--- djangoseattle/urls.py
+++ djangoseattle/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djangoseattle.views.home', name='home'),
-    # url(r'^djangoseattle/', include('djangoseattle.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='djangoseattle/index.html')),
+    url(r'^events/', TemplateView.as_view(template_name='djangoseattle/events.html')),
+    url(r'^admin/', include(admin.site.urls)),
 )
